c9ba6657ae58c0693ab0333574148fdc2c7cdd2f
deschlers <93087101+deschlers@users.noreply.github.com>
2025-02-13 01:18:58 -0500

    simple main menu

---
+++ Assets/Scripts/UnitButtons.cs
@@ -0,0 +1,25 @@
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UnitButtons : MonoBehaviour
+{
+    public void GoToUnit1() {
+        SceneManager.LoadScene("Scenes/Unit 1");
+    }
+    public void GoToUnit2() {
+        SceneManager.LoadScene("Scenes/Unit 2");
+    }
+    public void GoToUnit3() {
+        SceneManager.LoadScene("Scenes/Unit 3");
+    }
+    public void GoToUnit4() {
+        SceneManager.LoadScene("Scenes/Unit 4");
+    }
+    public void GoToUnit5() {
+        SceneManager.LoadScene("Scenes/Unit 5");
+    }
+    public void GoToUnit6() {
+        SceneManager.LoadScene("Scenes/Unit 6");
+    }
+}
