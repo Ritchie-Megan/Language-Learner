d65e0c6167a22fe3447655db4f8dcd8ba69acaee
august <boland.a@husky.neu.edu>
2025-03-13 12:08:00 -0400

    Syncing Unit 4 to github
    Sending to windows

--- Assets/Scripts/Unit 4/Movement.cs
+++ Assets/Scripts/Unit 4/Movement.cs
@@ -1,7 +1,7 @@
 using System;
 using UnityEngine;
 
-public class Movement : Photon.Pun.MonoBehaviourPun
+public class Movement : MonoBehaviour
 {
     private Vector2 velocity;
     private Rigidbody2D rb;
@@ -17,19 +17,14 @@ public class Movement : Photon.Pun.MonoBehaviourPun
 
     private void Update()
     {
-        if (photonView.IsMine)
-        {
-            velocity.x = Input.GetAxisRaw("Horizontal");
-            velocity.y = Input.GetAxisRaw("Vertical");
-        }
+        velocity.x = Input.GetAxisRaw("Horizontal");
+        velocity.y = Input.GetAxisRaw("Vertical");
 
     }
 
     private void FixedUpdate()
     {
-        if (photonView.IsMine)
-        {
-            rb.MovePosition(rb.position + (Time.fixedDeltaTime * speed * velocity));
-        }
+
+        rb.MovePosition(rb.position + (Time.fixedDeltaTime * speed * velocity));
     }
 }
