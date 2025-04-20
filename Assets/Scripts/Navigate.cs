36940568b16dca8c2aacfca2dbdd9c082336e345
deschlers <93087101+deschlers@users.noreply.github.com>
2025-04-20 16:02:38 -0400

    Minigame menus created
    Units 1, 2, 4, and 6 still need instructions.

--- Assets/Scripts/Navigate.cs
+++ Assets/Scripts/Navigate.cs
@@ -4,25 +4,44 @@ using UnityEngine.SceneManagement;
 
 public class Navigate : MonoBehaviour
 {
+    public void GoToMenu() {
+        SceneManager.LoadScene("Scenes/Main Menu");
+    }
+
     public void GoToUnit1() {
         SceneManager.LoadScene("Scenes/Unit 1");
+    }public void GoToUnit1Menu() {
+        SceneManager.LoadScene("Scenes/Unit 1 Menu");
     }
     public void GoToUnit2() {
         SceneManager.LoadScene("Scenes/Unit 2");
     }
+    public void GoToUnit2Menu() {
+        SceneManager.LoadScene("Scenes/Unit 2 Menu");
+    }
     public void GoToUnit3() {
+        SceneManager.LoadScene("Scenes/Unit 3");
+    }
+    public void GoToUnit3Menu() {
         SceneManager.LoadScene("Scenes/Unit 3 Menu");
     }
     public void GoToUnit4() {
         SceneManager.LoadScene("Scenes/Unit 4");
     }
+    public void GoToUnit4Menu() {
+        SceneManager.LoadScene("Scenes/Unit 4 Menu");
+    }
     public void GoToUnit5() {
         SceneManager.LoadScene("Scenes/Unit 5");
+    }
+    public void GoToUnit5Menu() {
+        SceneManager.LoadScene("Scenes/Unit 5 Menu");
     }
     public void GoToUnit6() {
         SceneManager.LoadScene("Scenes/Unit 6");
     }
-    public void GoToMenu() {
-        SceneManager.LoadScene("Scenes/Main Menu");
+    public void GoToUnit6Menu() {
+        SceneManager.LoadScene("Scenes/Unit 6 Menu");
     }
+    
 }
